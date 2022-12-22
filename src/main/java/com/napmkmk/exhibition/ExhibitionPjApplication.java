a9513e39c26e44b467540e0cab195ba2c44537ca
napmk <napmkmk@naver.com>
2022-12-22 17:35:32 +0900

    메인프론트작업중

---
+++ src/main/java/com/napmkmk/exhibition/ExhibitionPjApplication.java
@@ -0,0 +1,13 @@
+package com.napmkmk.exhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionPjApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/napmkmk/exhibition/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.napmkmk.exhibition.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = "/")
+	public String home() {
+		
+		return "redirect:index";
+	}
+	
+	
+	@RequestMapping(value = "/index")
+	public String index() {
+		
+		 
+		
+		return "index";
+	}
+
+}
